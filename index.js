bede6f668d1e70dddbbc5b7308c3b8f32ac925ea
Arsalan <arsalan475@gmail.com>
2024-08-13 15:54:54 +0500

    url change

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = express()
 
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: 'https://expensetrackerapp21.netlify.app/',
         
         credentials:true,
     }
