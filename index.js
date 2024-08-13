0f120e80ec640e3823d381d47cc749753d96a109
Arsalan <arsalan475@gmail.com>
2024-08-13 14:03:58 +0500

    layer file

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = express()
 
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: 'https://great-sad-mclaren.cidemo.co/',
         
         credentials:true,
     }
